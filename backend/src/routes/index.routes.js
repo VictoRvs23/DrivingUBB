f018a40b0fb7eb0b1a5febbade6ad7c6cbc26339
mati626 <matias.bustos2202@alumnos.ubiobio.cl>
2026-05-30 15:17:45 -0400

    integrar routes de evaluacionpractica en el router principal

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -3,10 +3,12 @@ import { Router } from "express";
 import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
 import claseteorica from "./claseteorica.routes.js";
+import evaluacionpractica from "./evaluacionpractica.routes.js";
 const router = Router();
 
 router.use("/users", userRoutes); 
 router.use("/auth", authRoutes);  
 router.use("/claseteorica", claseteorica);  
+router.use("/evaluacionpractica", evaluacionpractica);  
 
 export default router;
