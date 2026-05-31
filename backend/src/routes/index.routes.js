b00879858f5019d3bde141a89eab23e09f3f6b2c
mati626 <matias.bustos2202@alumnos.ubiobio.cl>
2026-05-30 20:32:20 -0400

    Agregar ruta para el examen teórico en el index.routes

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -4,11 +4,13 @@ import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
 import claseteorica from "./claseteorica.routes.js";
 import evaluacionpractica from "./evaluacionpractica.routes.js";
+import examenTeoricoRoutes from "./examenteorico.routes.js";
 const router = Router();
 
 router.use("/users", userRoutes); 
 router.use("/auth", authRoutes);  
 router.use("/claseteorica", claseteorica);  
 router.use("/evaluacionpractica", evaluacionpractica);  
+router.use("/examenteorico", examenTeoricoRoutes);  
 
 export default router;
