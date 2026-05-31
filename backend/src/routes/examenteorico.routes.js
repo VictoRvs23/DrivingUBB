aa6ad822b431928c7f633d8b002b0bf49031e3db
mati626 <matias.bustos2202@alumnos.ubiobio.cl>
2026-05-31 02:43:52 -0400

    Agregar rutas para obtener examen por ID y resultado del examen

--- backend/src/routes/examenteorico.routes.js
+++ backend/src/routes/examenteorico.routes.js
@@ -3,7 +3,9 @@ import{
     obtenerPreguntas,
     generarExamenAleatorio,
     guardarRespuestas,
-    finalizarExamen
+    finalizarExamen,
+    obtenerExamenPorId,
+    obtenerResultado
 }from "../controllers/examenteorico.controller.js";
 
 const examenTeoricoRoutes=Router();
@@ -19,5 +21,11 @@ examenTeoricoRoutes.put("/:id/respuestas",guardarRespuestas);
 
 //put finalizar examen
 examenTeoricoRoutes.put("/:id/finalizar",finalizarExamen);
+
+//get- obtener examen por id
+examenTeoricoRoutes.get("/:id",obtenerExamenPorId);
+
+//get- obtener resultado del examen
+examenTeoricoRoutes.get("/:id/resultado",obtenerResultado);
 
 export default examenTeoricoRoutes;
