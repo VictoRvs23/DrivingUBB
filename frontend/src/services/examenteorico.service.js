7705b5ee7f11046b31c1a62017068e673f721cdd
mati626 <matias.bustos2202@alumnos.ubiobio.cl>
2026-05-31 20:29:50 -0400

    agregado service para manejar exámenes teóricos y sus respuestas

---
+++ frontend/src/services/examenteorico.service.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+const api=axios.create({
+    baseURL:"http://localhost:3000/api",
+});
+
+//agregar token a cada request
+api.interceptors.request.use((config)=>{
+    const token=localStorage.getItem("token");
+    if(token){
+        config.headers.Authorization=`Bearer ${token}`;
+    }
+    return config;
+});
+
+//todas las preguntas
+export const obtenerPreguntasRequest = async () => {
+    return api.get('/preguntas');
+};
+
+//generar examen aleatorio
+export const generarExamenAleatorioRequest = async (tiempo_limite_segundos = 3600) => {
+    return api.post('/examenteorico', { tiempo_limite_segundos });
+};
+
+export const guardarRespuestasRequest = async (id_examen, respuestas_array) => {
+    return api.put(`/examenteorico/${id_examen}/respuestas`, {
+        respuestas_dada: respuestas_array
+    });
+};
+
+//finalizar examen 
+export const finalizarExamenRequest = async (id_examen) => {
+    return api.put(`/examenteorico/${id_examen}/finalizar`);
+};
+
+
+//obtener examen por id
+export const obtenerExamenPorIdRequest = async (id_examen) => {
+    return api.get(`/examenteorico/${id_examen}`);
+};
+
+//obtener resultado del examen
+export const obtenerResultadoRequest = async (id_examen) => {
+    return api.get(`/examenteorico/${id_examen}/resultado`);
+};
